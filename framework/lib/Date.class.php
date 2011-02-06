d416a145467ab95829889caf7a5d19f4bcf288cb
Tomo Krajina <puzz@puzz.(none)>
2011-02-06 20:25:29 +0100

    + Files::remove( $file )

--- framework/lib/Date.class.php
+++ framework/lib/Date.class.php
@@ -4,30 +4,30 @@ class Date extends Timestamp {
 
 	private $valid = true;
 
-    function __construct( $yearOrSqlString = null, $month = null, $day = null ) {
-    	if( $month != null && $day != null ) {
-	    	if( ! $yearOrSqlString ) {
-	    		$year = date( 'Y', microtime( true ) );
-	    	}
-	    	if( ! $month ) {
-	    		$month = date( 'n', microtime( true ) );
-	    	}
-	    	if( ! $day ) {
+	function __construct( $yearOrSqlString = null, $month = null, $day = null ) {
+		if( $month != null && $day != null ) {
+			if( ! $yearOrSqlString ) {
+				$year = date( 'Y', microtime( true ) );
+			}
+			if( ! $month ) {
+				$month = date( 'n', microtime( true ) );
+			}
+			if( ! $day ) {
 				$day = date( 'j', microtime( true ) );
-	    	}
-	    	$time = mktime( 12, 0, 0, $month, $day, $yearOrSqlString );
-	    	if( ! $time || $time == -1 ) {
-	    		$this->valid = false;
-	    	}
-    	}
-    	else {
-    		$time = strtotime( $yearOrSqlString );
-    		if( ! $time ) {
-    			$this->valid = false;
-    		}
-    	}
-    	parent::__construct( $time );
-    }
+			}
+			$time = mktime( 12, 0, 0, $month, $day, $yearOrSqlString );
+			if( ! $time || $time == -1 ) {
+				$this->valid = false;
+			}
+		}
+		else {
+			$time = strtotime( $yearOrSqlString );
+			if( ! $time ) {
+				$this->valid = false;
+			}
+		}
+		parent::__construct( $time );
+	}
 
 	public function after( $date ) {
 		if( ! is_object( $date ) ) {
