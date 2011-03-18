04bc00ae1a9b8f7587ce5a3562534b8fe5401b94
Tomo Krajina <puzz@puzz.(none)>
2011-03-18 09:47:20 +0100

    HtmlUtils: open{tag}, close{tag}, tag{tag} methods

--- framework/lib/utils/HtmlHelper.class.php
+++ framework/lib/utils/HtmlHelper.class.php
@@ -77,7 +77,31 @@ class HtmlHelper {
 		$this->opened = array();
 	}
 
-	public function __call( $method, $arguments ) {
+	function __call( $method, $arguments ) {
+		if( substr( $method, 0, 4 ) == 'open' ) {
+			$tag = strtolower( substr( $method, 4 ) );
+			if( sizeof( $arguments ) >= 2 ) {
+				return $this->open( $tag, $arguments[ 0 ], $arguments[ 1 ] );
+			}
+			if( sizeof( $arguments ) >= 1 ) {
+				return $this->open( $tag, $arguments[ 0 ] );
+			}
+			return $this->open( $tag );
+		}
+		if( substr( $method, 0, 5 ) == 'close' ) {
+			$tag = strtolower( substr( $method, 5 ) );
+			return $this->close( $tag );
+		}
+		if( substr( $method, 0, 3 ) == 'tag' ) {
+			$tag = strtolower( substr( $method, 3 ) );
+			if( sizeof( $arguments ) >= 2 ) {
+				return $this->tag( $tag, $arguments[ 0 ], $arguments[ 1 ] );
+			}
+			if( sizeof( $arguments ) >= 1 ) {
+				return $this->tag( $tag, $arguments[ 0 ] );
+			}
+			return $this->tag( $tag );
+		}
 		throw new AppException( 'Unknown method:', $method );
 	}
 
